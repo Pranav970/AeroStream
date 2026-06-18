500f85420aeb508f79515b3f657493376a8bc8fd
Pranav Rajput <92659078+Pranav970@users.noreply.github.com>
2026-06-18 10:22:15 +0530

    Update variables.tf

--- aerostream-pipeline/terraform/variables.tf
+++ aerostream-pipeline/terraform/variables.tf
@@ -1,1 +1,11 @@
+variable "aws_region" {
+  description = "The AWS region to deploy the infrastructure"
+  type        = string
+  default     = "us-east-1"
+}
 
+variable "project_name" {
+  description = "Base name for project resources"
+  type        = string
+  default     = "zeroscale"
+}
